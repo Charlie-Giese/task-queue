856c50f1f79f547d40e011085b1684ff9e5bf065
Charles-Giese <cmggiese@gmail.com>
2025-05-09 11:39:56 +0200

    formatting

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
-#include "task_queue.hpp"
+#include "include/task_queue.hpp"
+#include <iostream>
+#include <thread>
 
 int main() {
 
-    TaskQueue queue(4);
+  TaskQueue queue(4);
 
-    queue.enqueue([] {
-        std::cout << "Hello from thread " << std::this_thread::get_id() << "\n";
-    });
+  queue.enqueue([] {
+    std::cout << "Hello from thread " << std::this_thread::get_id() << "\n";
+  });
 
-    queue.enqueue([] {
-        std::cout << "Doing some work...\n";
-    });
+  queue.enqueue([] { std::cout << "Doing some work...\n"; });
 
-    return 0;
+  return 0;
 }
