d9442e277169c9b028d2a3d15f06db2748a759e4
Charles-Giese <cmggiese@gmail.com>
2025-05-12 12:42:30 +0200

    added queue order sanity check

--- tests/enqueue_tests.cpp
+++ tests/enqueue_tests.cpp
@@ -13,3 +13,22 @@ TEST(TaskQueueTest, EnqueuesAndExecutesTask) {
   std::this_thread::sleep_for(std::chrono::milliseconds(100));
   EXPECT_TRUE(flag);
 }
+
+TEST(TaskQueueTest, QueueOrder) {
+
+  TaskQueue queue(1);
+  std::vector<int> vec;
+  std::mutex mutex;
+
+  for (int i = 1; i <= 5; i++) {
+    queue.enqueue([&, i]() {
+      std::lock_guard<std::mutex> lock(mutex);
+      vec.push_back(i);
+    });
+  }
+
+  std::vector<int> expected_order = {1, 2, 3, 4, 5};
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  EXPECT_EQ(vec, expected_order);
+}
